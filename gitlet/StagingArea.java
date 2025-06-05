fe76c208672b2ebcd2d2d21c932ed0a0896305b7
Alexander Potocki <apotocki@griddynamics.com>
2025-06-05 18:48:11 +0200

    Change staging area to include name-file content mapping instead of name-sha1

--- gitlet/StagingArea.java
+++ gitlet/StagingArea.java
@@ -5,31 +5,31 @@ import java.util.Map;
 import static gitlet.Utils.*;
 
 public class StagingArea implements Dumpable {
-    private final HashMap<String, String> stagingArea;
+    private final HashMap<String, byte[]> stagingArea;
 
     public StagingArea() {
         stagingArea = new HashMap<>();
     }
 
-    public StagingArea(HashMap<String, String> stagingArea) {
+    public StagingArea(HashMap<String, byte[]> stagingArea) {
         this.stagingArea = stagingArea;
     }
 
-    public HashMap<String, String> getStagingArea() {
+    public HashMap<String, byte[]> getStagingArea() {
         return stagingArea;
     }
 
-    public static HashMap<String, String> getNewestStagingArea() {
+    public static HashMap<String, byte[]> getNewestStagingArea() {
         StagingArea sa = readObject(Repository.STAGING_AREA, StagingArea.class);
         return sa.getStagingArea();
     }
 
-    public static void saveStagingArea(HashMap<String, String> sa) {
+    public static void saveStagingArea(HashMap<String, byte[]> sa) {
         writeObject(Repository.STAGING_AREA, new StagingArea(sa));
     }
 
     public static void clearStagingArea() {
-        HashMap<String, String> stagingArea = getNewestStagingArea();
+        HashMap<String, byte[]> stagingArea = getNewestStagingArea();
         stagingArea.clear();
         saveStagingArea(stagingArea);
     }
